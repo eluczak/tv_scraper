5fdbde8a2ca952dc9ce5d6bb688117b5f69528a1
Ewelina <ewelina.luczak@gmail.com>
2022-03-01 00:20:08 +0100

    added channel number

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 num_of_subpages = 29
 
 fav_channels = ["Polsat","Polsat News","TVN 24","TVN","BBC World News","Polsat Film","Polsat Seriale","Kino Polska","TVN Fabuła","TV Puls","PULS 2","Comedy Central","Paramount Channel HD","AXN","AXN Spin","AXN White","AXN Black","FOX Comedy","FOX","Red Carpet TV HD","Warner TV HD","Ale kino+","TVN 7","TVN Style","BBC Lifestyle","BBC First","BBC Brit","BBC Earth","HISTORY","ZOOM TV","STOPKLATKA","Travel Channel","Tele 5 HD","Active Family","Biznes 24","CNN","TLC","Discovery Science","Discovery Historia","Animal Planet HD","Travel Channel","Planete+","Adventure","Fokus TV","CBS Reality","DocuBox HD","Da Vinci","CBS Europa","Sundance TV","Eurosport 1","Deutsche Welle","TV 5 Monde Europe","France 24"]
-
+fav_channels_numbers = ["5","322","6","7","343","481","498","70","107","8","25","10","11","22","477","479","480","94","95","162","486","482","9","106","109","110","111","382","109","26","27","104","119","163","323","347","355","377","391","381","383","384","385","386","392","397","398","491","494","565","814","817","818"]
 for j in range(1,num_of_subpages+1):
 
     # a whole subpage
@@ -23,5 +23,8 @@ for j in range(1,num_of_subpages+1):
         hour_soup = (soup_current[i].div).find("span", {"class": "hour"})
         title_soup = (soup_current[i].div).find("span", {"class": "title"})
         if(soup_channels[i].text.strip() in fav_channels):
-            st.write(hour_soup.text.strip() + " " + soup_channels[i].text.strip() + " - " + title_soup.a.text.strip())
-
+            hour = hour_soup.text.strip()
+            channel = soup_channels[i].text.strip()
+            programme = title_soup.a.text.strip()
+            channel_number = str(fav_channels_numbers[len(fav_channels) - fav_channels[::-1].index(soup_channels[i].text.strip()) - 1])
+            st.markdown(hour + " - " + channel + " **[ " + channel_number + " ]** " + programme)
